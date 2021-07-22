589159baf38f6890366d79d689a695d86ef69135
acar <77089894+acarfx@users.noreply.github.com>
2021-07-22 04:25:47 +0300

    Create acar.js

---
+++ acar.js
@@ -0,0 +1,6 @@
+Gençler öncelikle siz isimi değişip x sunucusna gidip dalga geçip daşşak geçip egoist gibi davranıp millete hava atın diye paylaşmıcam kusura bakmayın.
+Adam gibi arkadaşlıkla yaklaşan insanlara ben verdim guardı zaten kimse boşuna burda beklemesin direk bot için gelen değil samimi yaklaşan insanlara verdim dcyi de tamamiyle olmasa da saldım.
+
+Discord sikim de gram değil real arkadaşımdan daha iyi arkadaşlar edindiğim için bu siktiğimin platformundayım botda umrumda değil bir kaç kişiye tamamen botları salmış durumundayım o kuruyu ediyo.
+Zaten onu da gruplara çekip altyapısını versene dev hesap vericez diye kandırmaya başlamışsınız bile ssleriniz geliyo herneyse sadece paylaşacağım sistem bu saatten sonra Otomatik sunucu kurup IDleri kendi yerleştiren sistemdir.
+Ekstrası olarak Prof-main in mongosunu atarım muhtemelen ve extra olarak stat botu koyarım sırf boş kalmasın diye hadi eyw
